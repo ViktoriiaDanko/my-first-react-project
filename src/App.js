52598a8e751eadc4098bc7da129eb144644bf399
ViktoriiaDanko <danko.vik146@gmail.com>
2019-11-01 17:07:38 +0200

    static todolist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+
+import todos from './api/todos';
+import users from './api/users';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world</h1>
+    <h1>Static list of todos</h1>
+      <p>
+        <span>Todos: </span>
+        {todos.length}
+      </p>
+
+      <p>
+        <span>Users: </span>
+        {users.length}
+      </p>
     </div>
   );
 }
+
+function UserList(props) {
+  const listItems = users.map((user) =>
+   <li key = {user.id}>
+     {user}
+   </li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+ReactDOM.render (
+  <UserList users = {users} />,
+  document.querySelector('.App')
+);
 
 export default App;

433bb738bda3a1aefb95527863129a60157d1fd3
ViktoriiaDanko <danko.vik146@gmail.com>
2019-11-20 23:36:08 +0200

    add completed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import './App.css';
 
 import todos from './api/todos';
 import users from './api/users';
+import PropTypes from 'prop-types';
 
 function App(props) {
-  const listItems = props.users.map((user) =>
-   <li key = {user.id}>
-     {user.name}
-   </li>
-  );
+  const listItems = props.todos.map((todo) => {
+    const user = props.users.find(user => user.id === todo.userId);
+    const completed =  todo.completed ? 'Completed' : 'Not completed';
 
+  return (
+    <tr>
+      <td key={user.id}>
+        {user.name}
+      </td>
+      <td key={todo.Id}>
+        {todo.title} 
+      </td>
+      <td key={todo.Id}>{completed}</td>
+    </tr>
+  )
+  });
+    
   return (
     <div className="App">
     <h1>Static list of todos</h1>
@@ -28,6 +40,20 @@ function App(props) {
     </div>
     </div>
   );
-}
+};
+
+App.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      completed: PropTypes.bool,
+    })
+  ).isRequired,
+  users: PropTypes.arrayOf(
+    PropTypes.shape( {
+      name: PropTypes.string,
+    })
+  ).isRequired
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+import todos from './api/todos';
 import users from './api/users';
 
 ReactDOM.render(
-    <App users = {users} />, 
+    <App todos = {todos} users = {users} />, 
     document.getElementById('root')
    );
